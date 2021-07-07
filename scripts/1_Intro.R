61aaa1a95ac47bac931e15ba0f353d63d8169fb2
jklipel <klipeljoice@gmail.com>
2021-07-07 15:03:44 -0300

    Lendo Tabela

--- scripts/1_Intro.R
+++ scripts/1_Intro.R
@@ -8,5 +8,11 @@
 
 ## Como carregar uma tabela
 
+# 1- Quando a tabela estiver em formato .txt no seu diretório
+data <- read.table("nome_da_tabela.txt", h=T)
+
+# 2- Quando a tabela estiver em formato .csc no seu diretório
+
+data <- read.csv("nome_da_tabela.csv", h=T)
 
 ## 
