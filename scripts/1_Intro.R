de5bfcb9183202ea0a2b26b3af8ee5ade7627c0e
jklipel <klipeljoice@gmail.com>
2021-07-07 15:28:56 -0300

    passos

--- scripts/1_Intro.R
+++ scripts/1_Intro.R
@@ -27,3 +27,41 @@ names(data)
 ## Como saber a dimensão da tabela
 
 dim(data)
+
+## Saber a "classe" da tabela
+
+class(data)
+
+## Como excluir uma (ou +) coluna(s): Várias maneiras... aqui vai algumas
+
+data <- data[,c(-1,-2,-5)] 
+
+# ou
+
+data <- data[,-c(1:3)]
+
+# ou
+
+data <- data[,-c(1,5)]
+
+## Como excluir uma linha da tabela
+
+data <- data[-1,]
+
+# Lembrar que antes da vírgula é linha e depois da vírgula se refere a colunas.
+
+## Minha tabela é gingantesca, mas quero apenas 3 colunas dela
+
+data <- data[,c("coluna1","coluna2","coluna3")]
+
+## Minha tabela é gigantesca, mas tem linhas com NA e quero tirar essas linhas
+
+row.has.na <- apply(data, 1, function(x){any(is.na(x))}) #tirar NA
+sum(row.has.na)
+data <- data[!row.has.na,]
+
+# ou eu sei o nome da linha que devo tirar
+
+data<-data[!(data$Nome_coluna=="Nome_linha"),]
+
+
