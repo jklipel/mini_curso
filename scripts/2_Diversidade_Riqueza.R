48a0c5d4150e19bf300fe220c53180a7fa28abbf
jklipel <klipeljoice@gmail.com>
2021-07-09 15:12:31 -0300

    Beta

--- scripts/2_Diversidade_Riqueza.R
+++ scripts/2_Diversidade_Riqueza.R
@@ -61,3 +61,23 @@ hill_taxa(comm = dummy$abun, q = 1)
 
 # same as: vegan::diversity(x = dummy$abun, index = 'invsimpson')
 hill_taxa(comm = dummy$abun, q = 2)
+
+
+####### baselga
+
+library(betapart)
+
+comm <-decostand (comunidade2, method="pa")
+#comm[comm>0] <-1 
+
+## calculando o multiple site dissimilarity
+beta_nat_2<-beta.multi(comm, index.family="sorensen")
+beta_nat_2 #isso para saber a beta total, e o que ? tuenover e nestedness
+
+###extrair matriz de dist usando simpson #isso se turnover for maior
+beta_nat<- beta.pair(comm, index.family="sorensen") # par a par, matriz de distancia 
+class(beta_nat)
+beta_sim<-beta_nat[["beta.sim"]] ###objeto apenas com a matriz de simpson
+beta_sim # turnover
+class(beta_sim)
+length(beta_sim)
